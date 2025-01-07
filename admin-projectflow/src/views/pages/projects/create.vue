00ae72143177a96aa304316ecfff37c4e878cd71
Aryan Tyagi <aryantyagi296@gmail.com>
2025-01-07 21:46:30 +0530

    feat: added task module

--- admin-projectflow/src/views/pages/projects/create.vue
+++ admin-projectflow/src/views/pages/projects/create.vue
@@ -1,51 +1,90 @@
 <template>
   <div>
-    <section class="profile-page mt-3">
+    <section class="task-page mt-3">
       <div v-loading="detailLoading" class="container-fluid">
         <el-card>
-          <el-form  ref="projectForm" :model="projectForm" :rules="rules" label-width="auto" label-position="top">
+          <el-form ref="taskForm" :model="taskForm" :rules="rules" label-width="auto" label-position="top">
             <el-row :gutter="20">
               <el-col :md="12">
-                <el-form-item label="Name" prop="name">
-                  <el-input v-model="projectForm.name" type="text" />
+                <el-form-item label="Title" prop="title">
+                  <el-input v-model="taskForm.title" type="text" />
                 </el-form-item>
               </el-col>
               <el-col :md="12">
                 <el-form-item label="Description">
-                  <el-input v-model="projectForm.description" type="text" />
+                  <el-input v-model="taskForm.description" type="text" />
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row :gutter="20">
-                <el-col :md="12">
-                    <el-form-item label="Members" prop="members">
-                        <el-select v-model="projectForm.members" placeholder="Select Members" multiple>
-                        <el-option
-                            v-for="member in membersList"
-                            :key="member._id"
-                            :label="member.name"
-                            :value="member._id"
-                        ></el-option>
-                        </el-select>
-                    </el-form-item>
-                </el-col>
-                <el-col :md="12">
-                  <el-form-item label="Status" prop="status">
-                    <el-select v-model="projectForm.status" placeholder="Select Status">
-                      <el-option
-                        v-for="status in statusList"
-                        :key="status"
-                        :label="status"
-                        :value="status"
-                      ></el-option>
-                    </el-select>
-                  </el-form-item>
-                </el-col>
+              <el-col :md="12">
+                <el-form-item label="Project" prop="project">
+                  <el-select v-model="taskForm.project" placeholder="Select Project">
+                    <el-option
+                      v-for="project in projectList"
+                      :key="project._id"
+                      :label="project.name"
+                      :value="project._id"
+                    ></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :md="12">
+                <el-form-item label="Assigned To" prop="assignedTo">
+                  <el-select v-model="taskForm.assignedTo" placeholder="Assign User">
+                    <el-option
+                      v-for="member in membersList"
+                      :key="member._id"
+                      :label="member.name"
+                      :value="member._id"
+                    ></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :md="12">
+                <el-form-item label="Status" prop="status">
+                  <el-select v-model="taskForm.status" placeholder="Select Status">
+                    <el-option
+                      v-for="status in statusList"
+                      :key="status"
+                      :label="status"
+                      :value="status"
+                    ></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+              <el-col :md="12">
+                <el-form-item label="Priority" prop="priority">
+                  <el-select v-model="taskForm.priority" placeholder="Select Priority">
+                    <el-option
+                      v-for="priority in priorityList"
+                      :key="priority"
+                      :label="priority"
+                      :value="priority"
+                    ></el-option>
+                  </el-select>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="20">
+              <el-col :md="12">
+                <el-form-item label="Due Date" prop="dueDate">
+                  <el-date-picker
+                    v-model="taskForm.dueDate"
+                    type="date"
+                    placeholder="Select Due Date"
+                  ></el-date-picker>
+                </el-form-item>
+              </el-col>
             </el-row>
             <div class="d-flex align-items-center justify-content-center mt-4">
-              <el-button class="btn btn-primary" :disabled="addingRequest" @click="addProject('projectForm')">{{ $t('company.save') }}<span v-if="addingRequest"> ...</span></el-button>
+              <el-button class="btn btn-primary" :disabled="addingRequest" @click="addTask('taskForm')">
+                Save<span v-if="addingRequest"> ...</span>
+              </el-button>
               <router-link :to="'/'">
-                <el-button class="btn btn-secondary ms-3">{{ $t('company.cancel') }}</el-button>
+                <el-button class="btn btn-secondary ms-3">Cancel</el-button>
               </router-link>
             </div>
           </el-form>
@@ -56,79 +95,97 @@
 </template>
 
 <script>
-import { getMemberList, createProject, getProjectDetail, updateProject } from '@/api/project'
+import { getMemberList, getProjectList, createTask, getTaskDetail, updateTask } from '@/api/task'
+
 export default {
   page: {
-    title: 'Add Project',
+    title: 'Add Task',
   },
   data() {
     return {
       addingRequest: false,
       detailLoading: false,
       id: '',
+      projectList: [],
       membersList: [],
-      projectForm: {
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
-        members: [],
+      taskForm: {
+        title: '',
+        description: '',
+        project: '',
+        assignedTo: '',
+        status: '',
+        priority: '',
+        dueDate: '',
       },
-      statusList: ['Active', 'InActive', 'Hold', 'Archive'],
+      statusList: ['Not Started', 'In Progress', 'Ready For Review', 'Ready For Live', 'Completed', 'On Hold'],
+      priorityList: ['Low', 'Medium', 'High'],
       rules: {
-        name: [
-          { required: true, message: "Name is Required", trigger: 'change' }
+        title: [
+          { required: true, message: "Title is required", trigger: 'change' }
         ],
-        members: [
-          { required: true, message: "Members is Required", trigger: 'change' }
+        project: [
+          { required: true, message: "Project is required", trigger: 'change' }
         ],
         status: [
-          { required: true, message: "Status is Required", trigger: 'change' }
-        ]
-      }
-    }
+          { required: true, message: "Status is required", trigger: 'change' }
+        ],
+      },
+    };
   },
   created() {
-    this.detailLoading = true
-    getMemberList().then(response => {
-      this.membersList = response.data.data
-      this.detailLoading = false
-    }).catch(error => {
-      this.detailLoading = false
-      this.$message.error(error.message)
-    })
-    this.id = this.$route.params.id || ''
+    this.detailLoading = true;
+
+    // Fetch Members
+    getMemberList().then((response) => {
+      this.membersList = response.data.data;
+    }).catch((error) => {
+      this.$message.error(error.message);
+    });
+
+    // Fetch Projects
+    getProjectList().then((response) => {
+      this.projectList = response.data.data;
+      this.detailLoading = false;
+    }).catch((error) => {
+      this.detailLoading = false;
+      this.$message.error(error.message);
+    });
+
+    // Check if editing an existing Task
+    this.id = this.$route.params.id || '';
     if (this.id) {
-      getProjectDetail(this.id).then(response => {
-      this.projectForm = response.data.data
-      }).catch(error => {
-      this.$message.error(error.message)
-      })
+      getTaskDetail(this.id).then((response) => {
+        this.taskForm = response.data.data;
+      }).catch((error) => {
+        this.$message.error(error.message);
+      });
     }
   },
   methods: {
-    addProject(formName) {
+    addTask(formName) {
       this.$refs[formName].validate((valid) => {
-      if (valid) {
-        const { name, description, members, status, _id } = this.projectForm
-        const opts = { name, description, members, status }
-        this.addingRequest = true
-        const request = _id ? updateProject(_id, opts) : createProject(opts)
-        request.then(response => {
-        this.addingRequest = false
-        this.$router.push({ path: '/projects' })
-        this.$refs[formName].resetFields()
-        this.$message({ type: 'success', message: response.data.message })
-        }).catch(error => {
-        this.addingRequest = false
-        this.$message.error(error.message)
-        })
-      } else {
-        console.log('error submit!!')
-        return false
-      }
-      })
-    }
-  }
-}
+        if (valid) {
+          const { title, description, project, assignedTo, status, priority, dueDate } = this.taskForm;
+          const opts = { title, description, project, assignedTo, status, priority, dueDate };
+          this.addingRequest = true;
+
+          const request = this.id ? updateTask(this.id, opts) : createTask(opts);
+
+          request.then((response) => {
+            this.addingRequest = false;
+            this.$router.push({ path: '/tasks' });
+            this.$refs[formName].resetFields();
+            this.$message({ type: 'success', message: response.data.message });
+          }).catch((error) => {
+            this.addingRequest = false;
+            this.$message.error(error.message);
+          });
+        } else {
+          console.log('Validation failed!');
+          return false;
+        }
+      });
+    },
+  },
+};
 </script>
